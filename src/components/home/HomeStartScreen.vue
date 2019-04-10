f7af3203c24e7909cd8953d2ef056af43a098e79
Edigleysson Silva <edigleyssonsilva@outlook.com>
2019-04-10 11:44:14 -0300

    Homescreen changed
    - changed home image
    - what we do changed
    - added new icons for Node and Laravel

--- src/components/home/HomeStartScreen.vue
+++ src/components/home/HomeStartScreen.vue
@@ -3,8 +3,8 @@
         <v-layout wrap align-center justify-center row fill-height >
            <v-flex xs12 sm5 md3 lg3 xl3 class="text-xs-center pa-3">
                 <v-img
-                    :src="require('@/assets/img/gdg.jpg')"
-                    :lazy-src="require('@/assets/img/gdg.jpg')"
+                    :src="require('@/assets/img/svg/svg1.svg')"
+                    :lazy-src="require('@/assets/img/svg/svg1.svg')"
                     width="100%">
                     <v-layout
                         slot="placeholder"
@@ -18,22 +18,22 @@
                 </v-img>
             </v-flex> 
             <v-flex xs12 sm7 md8 lg8 xl8 class="">
-                <p class="google-font mb-1" style="font-weight: 350;color: #616161;font-size:300%"><b>Make good things 
-                    <span style="color: #1a73e8;">together</span>.</b>
+                <p class="google-font mb-1" style="font-weight: 350;color: #616161;font-size:300%"><b>Fazendo grandes coisas 
+                    <span style="color: #1a73e8;">juntos</span>.</b>
                 </p>
-                <p class="google-font mt-0" style="font-size:180%">{{chapterDetails.ChapterName}}</p>
-                <p class="google-font" style="font-size:110%">{{chapterDetails.ChapterShortDescription}}</p>
+                <!-- <p class="google-font mt-0" style="font-size:180%">{{chapterDetails.ChapterName}}</p> -->
+                <p class="google-font" style="font-size:110%">{{chapterDetails.chanadaPrincipal}}</p>
                 <p class="google-font" style="font-size:110%;color:#9e9e9e">
                     <span v-for="(item,i) in chapterDetails.ChapterHashTags" :key="i">
                         #{{item}} &nbsp;
                     </span>
                 </p>
 
-                <v-btn :href="chapterDetails.ChapterMeetupLink" target="_blank" class="ma-0 google-font elevation-1" color="#1a73e8" style="text-transform: capitalize;border-radius:5px;color:white">Become a Member</v-btn>
+                <v-btn :href="chapterDetails.ChapterMeetupLink" target="_blank" class="ma-0 google-font elevation-1" color="#1a73e8" style="text-transform: initial;border-radius:5px;color:white">Ir para o BLOG</v-btn>
 
                 &nbsp;
 
-                <v-btn :href="chapterDetails.GDGProgramWebsite" target="_blank" round color="cyan" style="text-transform: capitalize;border-radius:5px;text-transform: capitalize;" outline  class="ml-0" dark>Learn More</v-btn>
+                <!-- <v-btn :href="chapterDetails.GDGProgramWebsite" target="_blank" round color="cyan" style="text-transform: capitalize;border-radius:5px;text-transform: capitalize;" outline  class="ml-0" dark>Learn More</v-btn> -->
 
             </v-flex>
         </v-layout>
--- src/components/home/whatWeDo.vue
+++ src/components/home/whatWeDo.vue
@@ -2,10 +2,10 @@
     <v-container class="pa-0">
         <v-layout wrap align-center justify-center row fill-height >
            <v-flex xs12 md3 lg3 class="pa-2">
-               <p class="google-font" style="font-size:180%">What we do?</p>
-               <p class="google-font" style="font-size:110%">Google Developer Groups (GDGs) are for developers who are interested in Google's developer technology.</p>
+               <p class="google-font" style="font-size:180%">O que encontrará aqui?</p>
+               <p class="google-font" style="font-size:110%">Atuando na área há 7 anos, tenho focado mais no desenvolvimento de aplicações mobile e web.</p>
 
-               <p class="google-font" style="font-size:110%">About different Google technologies</p>
+               <p class="google-font" style="font-size:110%">Algumas tecnologias com que trabalhp</p>
 
                 <v-tooltip bottom>
                     <template v-slot:activator="{ on }">
@@ -25,53 +25,57 @@
                     </template>
                     <span>Android</span>
                 </v-tooltip>
-                <v-tooltip bottom>
-                    <template v-slot:activator="{ on }">
-                        <img src="@/assets/img/svg/aog.png" width="30vh" alt="" srcset="" dark v-on="on">&nbsp;&nbsp;&nbsp;
-                    </template>
-                    <span>Actions On Google</span>
-                </v-tooltip>
+              
                 <v-tooltip bottom>
                     <template v-slot:activator="{ on }">
                         <img src="@/assets/img/svg/firebase.svg" width="30vh" alt="" srcset="" dark v-on="on">&nbsp;&nbsp;&nbsp;
                     </template>
                     <span>Firebase</span>
                 </v-tooltip>
+
                 <v-tooltip bottom>
                     <template v-slot:activator="{ on }">
-                        <img src="@/assets/img/svg/tf.svg" width="30vh" alt="" srcset="" dark v-on="on">
+                        <img src="@/assets/img/svg/laravel.png" width="36vh" alt="" srcset="" dark v-on="on">&nbsp;&nbsp;&nbsp;
                     </template>
-                    <span>Tenserflow</span>
+                    <span>Laravel</span>
                 </v-tooltip>
+
+                <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                        <img src="@/assets/img/svg/nodejs.png" width="40vh" alt="" srcset="" dark v-on="on">&nbsp;&nbsp;&nbsp;
+                    </template>
+                    <span>NodeJS</span>
+                </v-tooltip>
+               
                 <br><br>
 
-                <v-btn :href="chapterDetails.GDGProgramWebsite" outline target="_blank" round color="cyan" style="text-transform: capitalize;border-radius:5px;text-transform: capitalize;" flat  class="ml-0 google-font hidden-md-and-down" dark>Learn More</v-btn>
+                <v-btn :href="chapterDetails.GDGProgramWebsite" outline target="_blank" round color="cyan" style="text-transform: capitalize;border-radius:5px;text-transform: capitalize;" flat  class="ml-0 google-font hidden-md-and-down" dark>Ver mais</v-btn>
 
             </v-flex> 
             <v-flex xs12 md9 lg9 class="">
                 <v-layout row wrap>
 
-                    <v-flex xs6 sm6 md3 lg3 class="text-xs-center pa-2">
+                    <v-flex xs6 sm6 md4 lg4 class="text-xs-center pa-2">
                         <v-icon large style="font-size:300%">chat_bubble</v-icon>
-                        <p class="google-font" style="font-size:130%">Talks</p>
+                        <p class="google-font" style="font-size:130%">Palestras</p>
                         <p class="google-font" style="font-size:110%">Get updated with the latest news and announcements</p>
                     </v-flex>
 
-                    <v-flex xs6 sm6 md3 lg3 class="text-xs-center pa-2">
+                    <v-flex xs6 sm6 md4 lg4 class="text-xs-center pa-2">
                         <v-icon large style="font-size:300%">code</v-icon>
-                        <p class="google-font" style="font-size:130%">Codelabs</p>
+                        <p class="google-font" style="font-size:130%">Exemplos de código</p>
                         <p class="google-font" style="font-size:110%">Get hands-on experience and guidance from the community members</p>
                     </v-flex>
-
+<!-- 
                     <v-flex xs6 sm6 md3 lg3 class="text-xs-center pa-2">
                         <v-icon large style="font-size:300%">school</v-icon>
                         <p class="google-font" style="font-size:130%">Campus Roadshows</p>
                         <p class="google-font" style="font-size:110%">Share knowledge in different Companies, colleges and universities</p>
-                    </v-flex>
+                    </v-flex> -->
 
-                    <v-flex xs6 sm6 md3 lg3 class="text-xs-center pa-2">
+                    <v-flex xs6 sm6 md4 lg4 class="text-xs-center pa-2">
                         <v-icon large style="font-size:300%">airplay</v-icon>
-                        <p class="google-font" style="font-size:130%">Live Viewing Parties</p>
+                        <p class="google-font" style="font-size:130%">Tutoriais em vídeo</p>
                         <p class="google-font" style="font-size:110%">Share knowledge in different Companies, colleges and universities</p>
                     </v-flex>
 
